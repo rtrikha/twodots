1133bf0acdc90ef901125cb6a21989d42a38833c
rtikha <raunak@guesthouser.com>
2020-05-09 13:45:44 +0530

    adding container boxes

---
+++ script.js
@@ -0,0 +1,13 @@
+var initCursor = new NodeCursor({
+  cursor : true, 
+  node : true, 
+  cursor_velocity : 1, 
+  node_velocity : 0.35, 
+  native_cursor : 'none', 
+  element_to_hover : '.nodeHover', 
+  cursor_class_hover : 'disable', 
+  node_class_hover : 'expand', 
+  hide_mode : true, 
+  hide_timing : 2000, 
+});
+
