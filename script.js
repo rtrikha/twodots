57880c67af85b1d5992d2958f375cb2a372675b5
rtikha <raunak@guesthouser.com>
2020-05-15 18:37:19 +0530

    all changes done, ready to be deployed

--- script.js
+++ script.js
@@ -1,44 +1,6 @@
 
-const app = {
-    pages: [],
-    show: new Event('show'),
-    init: function(){
-        app.pages = document.querySelectorAll('.page');
-        app.pages.forEach((pg)=>{
-            pg.addEventListener('show', app.pageShown);
-        })
-        
-        document.querySelectorAll('.nav-link').forEach((link)=>{
-            link.addEventListener('click', app.nav);
-        })
-        history.replaceState({}, 'Home', '#home');
-        window.addEventListener('popstate', app.poppin);
-    },
-    nav: function(ev){
-        ev.preventDefault();
-        let currentPage = ev.target.getAttribute('data-target');
-        document.querySelector('.active').classList.remove('active');
-        document.getElementById(currentPage).classList.add('active');
-        console.log(currentPage)
-        history.pushState({}, currentPage, `#${currentPage}`);
-        document.getElementById(currentPage).dispatchEvent(app.show);
-    },
-    pageShown: function(ev){
-        console.log('Page', ev.target.id, 'just shown');
 
-    },
-    poppin: function(ev){
-        console.log(location.hash, 'popstate event');
-        let hash = location.hash.replace('#' ,'');
-        document.querySelector('.active').classList.remove('active');
-        document.getElementById(hash).classList.add('active');
-        console.log(hash)
-        //history.pushState({}, currentPage, `#${currentPage}`);
-        document.getElementById(hash).dispatchEvent(app.show);
-    }
-}
 
-document.addEventListener('DOMContentLoaded', app.init);
 
 document.getElementById('move-footer').style.animationIterationCount=1;
 
