698d43c95438b31be53ca70daa3ac842c0a30222
rtikha <raunakstrikha@gmail.com>
2020-05-26 01:51:04 +0530

    adding reponsive css

--- script.js
+++ script.js
@@ -1,24 +1,8 @@
 
-document.getElementById('move-footer').style.animationIterationCount=1;
 
 function showMainContent() {
         document.getElementById("blackbox-supporter").style.display = "block";
         document.getElementById("blackbox").style.animation = "show-more 1s ease forwards";
 }
-//setTimeout(showMainContent, 5000);
-
-//classToggle();
 
 function showRestContent() {}
-
-
-var tooltips = document.querySelectorAll(".tooltip-span");
-
-window.onmousemove = function (e) {
-        var x = e.clientX + 0 + "px",
-                y = e.clientY + 100 + "px";
-        for (var i = 0; i < tooltips.length; i++) {
-                tooltips[i].style.top = y;
-                tooltips[i].style.left = x;
-        }
-};
