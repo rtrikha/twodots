7ff4352636dca1ce5bec62fcbd74a134c08375b5
rtikha <raunakstrikha@gmail.com>
2020-05-29 00:04:12 +0530

    adding firebase script

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 
-
 function showMainContent() {
         document.getElementById("blackbox-supporter").style.display = "block";
         document.getElementById("blackbox").style.animation = "show-more 1s ease forwards";
