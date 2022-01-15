9c68587359581b1341369d762cd3e3da160d2dc2
rtrikha <raunakstrikha@gmail.com>
2022-01-15 19:23:21 +0530

    removing blackbox

--- js/script.js
+++ js/script.js
@@ -1,6 +1,1 @@
-function showMainContent() {
-	document.getElementById('blackbox-supporter').style.display = 'block';
-	document.getElementById('blackbox').style.animation = 'show-more 1s ease forwards';
-}
 
-function showRestContent() {}
